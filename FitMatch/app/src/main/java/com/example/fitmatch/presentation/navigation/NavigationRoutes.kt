fb718a89fa75fa30dd6d513f553ec09bb070d18a
David Rodriguez <rodriguezj.david@javeriana.edu.co>
2025-09-07 21:02:00 -0500

    Proyecto con MVVM y navbar de prueba

---
+++ FitMatch/app/src/main/java/com/example/fitmatch/presentation/navigation/NavigationRoutes.kt
@@ -0,0 +1,9 @@
+package com.example.fitmatch.presentation.navigation
+
+object NavigationRoutes {
+    const val HOME = "home"
+    const val SEARCH = "search"
+    const val INBOX = "inbox"
+    const val NOTIFICATIONS = "notifications"
+    const val PROFILE = "profile"
+}
