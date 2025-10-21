70c199702bc2c22393d8b288a6fa037be60d827b
David Rodriguez <rodriguezj.david@javeriana.edu.co>
2025-10-21 16:23:26 -0500

    Update VendedorProfile.kt

--- FitMatch/app/src/main/java/com/example/fitmatch/model/user/VendedorProfile.kt
+++ FitMatch/app/src/main/java/com/example/fitmatch/model/user/VendedorProfile.kt
@@ -1,10 +1,10 @@
 package com.example.fitmatch.model.user
 
 data class VendedorProfile(
-    val userId: String,
-    val storeName: String,
-    val storeDescription: String,
-    val rating: Float,
-    val followers: Int,
-    val following: Int
+    val userId: String = "",
+    val storeName: String = "",
+    val storeDescription: String = "",
+    val rating: Float = 0f,
+    val followers: Int = 0,
+    val following: Int = 0
 )
