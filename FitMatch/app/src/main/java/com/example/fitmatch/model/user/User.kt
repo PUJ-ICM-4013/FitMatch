9de353aad09bba3c253fef9b6375ccfc0403fc3d
David Rodriguez <rodriguezj.david@javeriana.edu.co>
2025-10-21 19:57:05 -0500

    Update User.kt

--- FitMatch/app/src/main/java/com/example/fitmatch/model/user/User.kt
+++ FitMatch/app/src/main/java/com/example/fitmatch/model/user/User.kt
@@ -3,15 +3,15 @@ package com.example.fitmatch.model.user
 import com.google.firebase.Timestamp
 
 data class User(
-    val id: String = "",
-    val email: String = "",
-    val fullName: String = "",
-    val birthDate: String = "",
-    val city: String = "",
-    val gender: String? = null,
+    val id: String,
+    val email: String,
+    val fullName: String,
+    val birthDate: String,
+    val city: String,
+    val gender: String,
     val role: String, // "Cliente" | "Vendedor"
     val avatarEmoji: String,
-    val phone: String? = null,
+    val phone: String?,
     val createdAt: Timestamp,
     val updatedAt: Timestamp
 )
